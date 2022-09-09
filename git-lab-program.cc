b18e576d573baf5bb36e50d1a8fb7935ee7145a6
Maddi Raisch <mr593719@ohio.edu>
2022-09-09 15:55:32 -0400

    lab2 metadata changes

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Maddi Raisch
+ *        Date: 9/09/2022
+ * Description: C++ main function
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
